2aba3cab567d3d01125f8e80d898b4207a3604c6
Lucas-Masaba <masabaluke@gmail.com>
2021-12-16 23:55:40 +0300

    add styling

--- src/modules/api.js
+++ src/modules/api.js
@@ -32,6 +32,15 @@ const fetchDataFromAPI = async () => {
     )
     .join('');
   UL.innerHTML = values;
+  const scoreList = [...document.querySelectorAll('li')];
+
+  scoreList.forEach((item, index) => {
+    if (index % 2 === 0) {
+      item.classList.add('gray');
+    } else {
+      item.classList.add('whiteSmoke');
+    }
+  });
 };
 
 export default { submitScore, fetchDataFromAPI };
